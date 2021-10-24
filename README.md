c6b46199691379e2e832f0b95f7ed2a0488ea3b6
scarlettspade <walls.takeira@gmail.com>
2021-10-23 23:43:50 -0400

    I added all the NFT character data and images, and deployed my smart contract to the Rinkeby testnet.

--- README.md
+++ README.md
@@ -1,15 +1,27 @@
-# Basic Sample Hardhat Project
+# NFT BROWSER GAME
 
-This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, a sample script that deploys that contract, and an example of a task implementation, which simply lists the available accounts.
+This is a Buildspace project.
 
-Try running some of the following tasks:
+# ABOUT
 
-```shell
-npx hardhat accounts
-npx hardhat compile
-npx hardhat clean
-npx hardhat test
-npx hardhat node
-node scripts/sample-script.js
-npx hardhat help
-```
+I coded and deployed an Ethereum smart contract with playable NFT characters and an interactive web app using 
+Solidity and React amongst 200+ developers. As a Buildspace hackathon, there was a live "kick-off" stream showcasing an intro to the project scope.
+
+# USE CASE
+
+For my project, I used HBO's Lovecraft Country as a game theme. Users can mint NFTs as playable game characters from the show and attack the Shoggoths by connecting their Metamask wallet to the free Rinkeby testnet.
+
+# HELPFUL TERMS
+Smart Contract: a piece of code that lives on the blockchain
+Blockchain: a public place where anyone can securely read and write data for a fee
+Metamask Wallet: a crypto wallet & secure gateway to connect blockchain apps
+Rinkeby Testnet: a free Ethereum test network
+NFT: a data "token" that a person can own
+Mint: publicly publishing to the blockchain
+Opensea Marketplace: a peer-to-peer marketplace to buy, sell, and auction NFTs
+
+# INSTALL REQUIREMENTS
+Install node/npm
+Install Hardhat
+Install Hardhat Dependencies: Waffle & Ethers
+Install Metamask Browser Extension
--- contracts/LovecraftGame.sol
+++ contracts/LovecraftGame.sol
@@ -2,11 +2,146 @@
 
 pragma solidity ^0.8.0;
 
-import "hardhat/console.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract LovecraftGame is Ownable {
-    constructor() {
-        console.log("This is my game contract, flames!");
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "./libraries/Base64.sol";
+
+import "hardhat/console.sol";
+
+contract LovecraftGame is ERC721, Ownable {
+
+    struct CharacterAttributes {
+        uint characterIndex;
+        string name;
+        string imageURI;
+        string description;
+        uint hp;
+        uint maxHp;
+        uint attackDamage;
+        uint stamina;
+        uint intellect;
+        uint luck;
+        uint spirit;
+        uint magic;
     }
+
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIDs;
+
+    CharacterAttributes[] defaultCharacters;
+
+    // Creates a mapping from an NFTs' tokenId to that NFTs' character attributes.
+    mapping(uint256 => CharacterAttributes) public nftHolderAttributes;
+
+    // Creates a mapping from an NFT holders' address to their NFT tokenId.
+    mapping (address => uint256) public nftHolders;
+
+    constructor(
+        string[] memory characterNames,
+        string[] memory characterImageURIs,
+        string[] memory characterDescriptions,
+        uint[] memory characterHp,
+        uint[] memory characterAttackDmg,
+        uint[] memory characterStamina,
+        uint[] memory characterIntellect,
+        uint[] memory characterLuck,
+        uint[] memory characterSpirit,
+        uint[] memory characterMagic
+    ) 
+        ERC721("LOVECRAFT", "LC")
+    {
+        for(uint i = 0; i < characterNames.length; i += 1) {
+        defaultCharacters.push(CharacterAttributes({
+        characterIndex: i,
+        name: characterNames[i],
+        imageURI: characterImageURIs[i],
+        description: characterDescriptions[i],
+        hp: characterHp[i],
+        maxHp: characterHp[i],
+        attackDamage: characterAttackDmg[i],
+        stamina: characterStamina[i],
+        intellect: characterIntellect[i],
+        luck: characterLuck[i],
+        spirit: characterSpirit[i],
+        magic: characterMagic[i]
+      }));
+
+        CharacterAttributes memory c = defaultCharacters[i];
+        console.log("Done initializing %s w/ HP %s, img %s", c.name, c.hp, c.imageURI);
+    }
+
+        _tokenIDs.increment();
+  }
+
+  function tokenURI(uint256 _tokenId) public view override returns (string memory) {
+  CharacterAttributes memory charAttributes = nftHolderAttributes[_tokenId];
+
+  string memory strHp = Strings.toString(charAttributes.hp);
+  string memory strMaxHp = Strings.toString(charAttributes.maxHp);
+  string memory strAttackDamage = Strings.toString(charAttributes.attackDamage);
+  string memory strStamina = Strings.toString(charAttributes.stamina);
+  string memory strIntellect = Strings.toString(charAttributes.intellect);
+  string memory strLuck = Strings.toString(charAttributes.luck);
+  string memory strSpirit = Strings.toString(charAttributes.spirit);
+  string memory strMagic = Strings.toString(charAttributes.magic);
+
+  string memory json = Base64.encode(
+    bytes(
+      string(
+        abi.encodePacked(
+          '{"name": "',
+          charAttributes.name,
+          ' -- NFT #: ',
+          Strings.toString(_tokenId),
+          '", "description": "',
+          charAttributes.description, 
+          '", "image": "',
+          charAttributes.imageURI,
+          '", "attributes": [ { "trait_type": "Health Points", "value": ',strHp,', "max_value":',strMaxHp,'}, { "trait_type": "Attack Damage", "value": ',
+          strAttackDamage,'}, { "display_type": "boost_percentage", "trait_type": "Stamina", "value": ',strStamina,'}, { "display_type": "boost_percentage", "trait_type": "Intellect", "value": ',strIntellect,'}, { "display_type": "boost_percentage", "trait_type": "Luck", "value": ',strLuck,'}, { "display_type": "boost_percentage", "trait_type": "Spirit", "value": ',strSpirit,'}, { "display_type": "boost_percentage", "trait_type": "Magic", "value": ',strMagic,'} ]}'
+        )
+      )
+    )
+  );
+
+  string memory output = string(
+    abi.encodePacked("data:application/json;base64,", json)
+  );
+  
+  return output;
+}
+  
+  // This is the user function to mint character NFTs based on character selection.
+  function mintCharacter(uint _characterIndex) external {
+      uint256 newItemId = _tokenIDs.current();
+
+      // This assigns the tokenIDs to the user's address.
+      _safeMint(msg.sender, newItemId);
+
+      // Creates a mapping from a users' NFT tokenId to that NFTs character attributes.
+      nftHolderAttributes[newItemId] = CharacterAttributes({
+          characterIndex: _characterIndex,
+          name: defaultCharacters[_characterIndex].name,
+          imageURI: defaultCharacters[_characterIndex].imageURI,
+          description: defaultCharacters[_characterIndex].description,
+          hp: defaultCharacters[_characterIndex].hp,
+          maxHp: defaultCharacters[_characterIndex].maxHp,
+          attackDamage: defaultCharacters[_characterIndex].attackDamage,
+          stamina: defaultCharacters[_characterIndex].stamina,
+          intellect: defaultCharacters[_characterIndex].intellect,
+          luck: defaultCharacters[_characterIndex].luck,
+          spirit: defaultCharacters[_characterIndex].spirit,
+          magic: defaultCharacters[_characterIndex].magic
+      });
+
+      console.log("Minted NFT w/ tokenId %s and characterIndex %s", newItemId, _characterIndex);
+
+      // This displays NFT owners' addresses.
+      nftHolders[msg.sender] = newItemId;
+
+      _tokenIDs.increment();
+  }
 }
